fabd63c02485b1a1f663be976cc7d8690c935523
Lucas <lucas.98sf@gmail.com>
2021-09-27 23:57:59 -0300

    add editorconfig

--- bin/www.js
+++ bin/www.js
@@ -10,7 +10,7 @@ app.set('port', port);
 
 const server = http.createServer(app);
 
-server.listen(port, () => console.log('Server running on port ' + port));
+server.listen(port, () => console.log('Server running http://localhost:' + port));
 server.on('error', onError);
 server.on('listening', onListening);
 
