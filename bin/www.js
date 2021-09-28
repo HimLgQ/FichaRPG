4aa3b03cb1f96327fadf25a2883ae18592f64a0a
Lucas <lucas.98sf@gmail.com>
2021-09-28 01:39:40 -0300

    add gulp e browsersync

--- bin/www.js
+++ bin/www.js
@@ -5,12 +5,12 @@ import http from 'http';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const port = process.env.PORT || '3000';
+const port = Number(process.env.PORT) || 3000;
 app.set('port', port);
 
 const server = http.createServer(app);
 
-server.listen(port, () => console.log('Server running http://localhost:' + port));
+server.listen(port, () => console.log('Server running on http://localhost:' + (port + 1)));
 server.on('error', onError);
 server.on('listening', onListening);
 
---
+++ gulpfile.babel.js
@@ -0,0 +1,33 @@
+import gulp from 'gulp';
+import browserSync from 'browser-sync';
+import nodemon from 'gulp-nodemon';
+
+const port = Number(process.env.PORT) || 3000;
+const browserOpts = {
+    proxy: 'http://localhost:' + port,
+    port: port + 1,
+    open: false
+};
+
+gulp.task('development', () => {
+    let restarted = false;
+    browserSync.create();
+    browserSync.init(browserOpts);
+
+    return nodemon({
+        exec: 'babel-node --presets=@babel/preset-env ./bin/www.js',
+        inspect: '9228',
+        ignore: './node_modules',
+        ext: 'js,ejs,json,css'
+    })
+    .on('start', () => {
+        if (restarted) {
+            setTimeout(() => {
+                browserSync.reload();
+            }, 1000);
+        }
+    })
+    .on('restart', () => {
+        restarted = true;
+    });
+});
